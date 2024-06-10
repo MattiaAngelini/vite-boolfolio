85d08b9436a0487e5e033446cb98d8856b5caa48
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-06-10 17:11:54 +0200

    feat: printed all cards from API call

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,12 +1,45 @@
 <script>
+import axios from 'axios';
 
 export default {
-    name: 'ProjectList'
+    name: 'ProjectList',
+
+    data() {
+        return {
+            projects: []
+        };
+    },
+    methods: {
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+            .then((response) => {
+                this.projects = response.data.results;
+            });
+        }
+    },
+    mounted() {
+        this.getProjects();
+    }
 }
 </script>
 
 <template>
-    <h1>Projects list</h1>
+    <h1 class="text-center">Elenco dei Progetti:</h1>
+
+    <section class="d-flex flex-wrap gap-2">
+
+        <div v-for="project in projects">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ project.name }}</h5>
+                    <p v-if="project.summary" class="card-text">{{ project.summary }}</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
 </template>
 
 <style scoped lang="scss">
